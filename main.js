6461e1f0534a71379c6a7338ae57e8039f58c7ba
GastonOrquera <gastonorquera29@gmail.com>
2024-12-03 17:04:13 -0300

    responsive

--- main.js
+++ main.js
@@ -3,3 +3,17 @@ let arrow = document.querySelector('.arrow');
 arrow.addEventListener('click', () => {
     window.location.hash = 'servicios';
 });
+
+let hamburger = document.querySelector('.hamburger');
+let nav = document.querySelector('nav');
+let navLinks = document.querySelectorAll('a');
+
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+
+    navLinks.forEach((links) => {
+        links.addEventListener('click', () => {
+            nav.classList.remove('active');
+        })
+    })
+});
